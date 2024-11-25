b15500375f1ad5cb8cc0139456c0b3d616c309ab
Abid Mahmud <94243005+Adi14344@users.noreply.github.com>
2024-11-25 13:33:31 +0600

    Add files via upload
    commit 1st

---
+++ mid lab/home_page.php
@@ -0,0 +1,18 @@
+
+<?php
+    session_start();
+    if(!isset($_SESSION['status'])){
+        header('location: login_page.html');  
+    }
+?>
+
+
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+<body>
+        <h1>Welcome Home! <?=$_SESSION['username']?></h1>    
+        <a href="logout_page.php"> logout </a>
+</body>
+</html>
---
+++ mid lab/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Signin & Signup</title>
+</head>
+<body>
+	<!-- <img src="brazil (1).jpg" width="1200px" height="200px"> -->
+	<dev class="login">
+		<fieldset>
+			<legend><h1>login</h1></legend>
+				<center>
+				<form>
+				
+					Email or Number: <input type="email" name="" value="" required>
+						<br></br>
+					</br>Password: <input type="password" name="" value="" required>
+						<br></br>
+					<input type="submit" name="" value="submit">
+				</form>
+					<p> Not have a account? <b><a href="Signup_page.html"> Signup here!</a></b> </p>
+				</center>
+		</fieldset>
+</body>
+</html>
